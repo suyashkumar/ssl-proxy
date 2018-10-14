439d847132ede2a71d9b4d36092169d489f7362c
Suyash Kumar <suyashkumar2003@gmail.com>
2018-10-14 00:12:36 -0400

    Add forwarding headers on incoming requests

--- main.go
+++ main.go
@@ -79,8 +79,14 @@ func main() {
 		log.Fatal("Unable to parse 'to' url: ", err)
 	}
 
-	// Setup ServeMux
-	localProxy := httputil.NewSingleHostReverseProxy(toURL)
+	// Setup reverse proxy ServeMux
+	localProxy := &httputil.ReverseProxy{}
+	addProxyHeaders := func(req *http.Request) {
+		req.Header.Set(http.CanonicalHeaderKey("X-Forwarded-Proto"), "https")
+		req.Header.Set(http.CanonicalHeaderKey("X-Forwarded-Port"), "443")
+	}
+	localProxy.Director = newDirector(toURL, addProxyHeaders)
+
 	mux := http.NewServeMux()
 	mux.Handle("/", localProxy)
 
@@ -109,4 +115,44 @@ func main() {
 	} else {
 		log.Fatal(http.ListenAndServeTLS(*fromURL, *certFile, *keyFile, mux))
 	}
+}
+
+// newDirector creates a base director that should be exactly what http.NewSingleHostReverseProxy() creates, but allows
+// for the caller to supply and extraDirector function to decorate to request to the downstream server
+// TODO: add test to ensure behavior does not diverge from httputil's implementation, as per Rob Pike's proverbs
+func newDirector(target *url.URL, extraDirector func(*http.Request)) func(*http.Request) {
+	targetQuery := target.RawQuery
+	return func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+
+		if extraDirector != nil {
+			extraDirector(req)
+		}
+	}
+}
+
+// singleJoiningSlash is a utility function that adds a single slash to a URL where appropriate, copied from
+// the httputil package
+// TODO: add test to ensure behavior does not diverge from httputil's implementation, as per Rob Pike's proverbs
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
